3bfb6b0b34b6124218785625b1496806a04c7738
jany-coder <jany_ete@yahoo.com>
2021-10-12 13:03:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// booking rooms
+
+function handleBooking() {
+    var x = document.getElementById("bookingRooms");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
